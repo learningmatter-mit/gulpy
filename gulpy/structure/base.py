5786d8336dc96585aaba4982a7e6d73a70d4ca42
Daniel Schwalbe-Koda <dskoda@mit.edu>
2020-05-12 07:28:40 -0400

    add core/shell to labels

--- gulpy/structure/base.py
+++ gulpy/structure/base.py
@@ -8,6 +8,18 @@ class GulpObject:
     def get_shells(self):
         raise NotImplementedError
 
+    def get_labels_with_shells(self):
+        labels = self.get_labels()
+        shells = self.get_shells()
+
+        labels_with_shells = []
+        core_shell = []
+        for label, has_shell in zip(labels, shells):
+            labels_with_shells += [label] * (2 if has_shell else 1)
+            core_shell += ['core', 'shell'] if has_shell else ['core']
+
+        return labels_with_shells, core_shell
+
     def get_structure(self):
         raise NotImplementedError
 
--- gulpy/structure/tests/test_crystal.py
+++ gulpy/structure/tests/test_crystal.py
@@ -3,7 +3,7 @@ import unittest as ut
 from pymatgen.core import Structure
 
 from gulpy.structure import GulpCrystal
-from gulpy.structure.labels import DreidingLabels
+from gulpy.structure.labels import DreidingLabels, CatlowLabels
 from gulpy.tests.test_files import load_structure, load_molecule
 
 
@@ -19,6 +19,15 @@ class TestGulpCrystal(ut.TestCase):
     def test_bonds(self):
         bonds = [(1, 13, 'single'), (1, 21, 'single'), (1, 17, 'single'), (1, 9, 'single'), (2, 14, 'single'), (2, 22, 'single'), (2, 18, 'single'), (2, 10, 'single'), (3, 9, 'single'), (3, 15, 'single'), (3, 23, 'single'), (3, 19, 'single'), (4, 10, 'single'), (4, 16, 'single'), (4, 24, 'single'), (4, 20, 'single'), (5, 11, 'single'), (5, 19, 'single'), (5, 15, 'single'), (5, 22, 'single'), (6, 12, 'single'), (6, 20, 'single'), (6, 16, 'single'), (6, 21, 'single'), (7, 17, 'single'), (7, 13, 'single'), (7, 24, 'single'), (7, 11, 'single'), (8, 18, 'single'), (8, 14, 'single'), (8, 23, 'single'), (8, 12, 'single')]
         self.assertEqual(self.gcrys.get_bonds(), bonds)
+
+    def test_labels_with_shell(self):
+        gcrys = GulpCrystal(self.struct, CatlowLabels())
+
+        exp_labels = ['Si'] * 8 + ['O_O2-'] * (16 * 2)
+        exp_core_shell = ['core'] * 8 + ['core', 'shell'] * 16
+        labels, core_shell = gcrys.get_labels_with_shells()
+        self.assertEqual(labels, exp_labels)
+        self.assertEqual(core_shell, exp_core_shell)
 
 
 if __name__ == "__main__":
