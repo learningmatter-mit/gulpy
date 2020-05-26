6d1e6a3c39a2ed587d01d4768f7c234aa9b8aa48
Daniel Schwalbe-Koda <dskoda@mit.edu>
2020-05-26 07:22:37 -0400

    add test for optimization job

---
+++ gulpy/jobs/tests/test_opti.py
@@ -0,0 +1,24 @@
+import unittest as ut
+
+from gulpy.jobs import OptiJob
+from gulpy.inputs.tests.test_library import ExampleLibrary
+from gulpy.structure import GulpCrystal
+from gulpy.structure.labels import DreidingLabels
+from gulpy.tests.test_files import load_structure, get_jobs_path
+
+
+class TestJob(ut.TestCase):
+    def setUp(self):
+        self.job = OptiJob(None, None)
+
+    def test_parse(self):
+        results = self.job.parse_results(get_jobs_path("opti/opti.out"))
+
+        self.assertAlmostEqual(results['gnorm'], 0.06196731)
+        self.assertAlmostEqual(results['energy'], 64.96556639)
+        self.assertAlmostEqual(results['duration'], 22.8976)
+        self.assertEqual(results['version'], '5.1.1')
+
+
+if __name__ == "__main__":
+    ut.main()
